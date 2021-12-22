95d77960d3eff3ed225a5969e598bfaef98c89ac
gabisegger <gabi.segger@gmail.com>
2021-12-22 19:39:26 -0300

    chore: ajustes

--- js/action.js
+++ js/action.js
@@ -1,3 +1,19 @@
-var pageLink = window.location.href;
-var splitPageLink = pageLink.split('/').pop();
-$('body').addClass(splitPageLink);
+//
+if(jQuery(window).width() < 760){
+    //breadcrumb
+    if(jQuery('.page-central_lista_espera').length){
+        jQuery('.breadcrumb.central-breadcrumb').prependTo('.site-main.container');
+    }
+    //fim breadcrumb
+   
+    //posts-blog
+    jQuery('.blog-posts').slick();
+    //fim posts-blog
+   
+    
+    jQuery('.products-related > .row').slick();
+}
+
+
+
+
